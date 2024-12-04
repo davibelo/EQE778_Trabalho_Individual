ae11ede390e036944d7662366e6136eae545f416
Davi Belo <davibelo@gmail.com>
2024-12-04 18:09:00 -0300

    try

--- 10-optimize.py
+++ 10-optimize.py
@@ -1,10 +1,11 @@
 import os
 import joblib
-import numpy as np
 import logging
 import win32com.client as win32
-from scipy.optimize import minimize
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.interpolate import griddata
 
@@ -20,32 +21,40 @@ logging.basicConfig(
 )
 
 # Model and scaler identification
-MODEL_ID = '09'
+MODEL_ID = '10'
 
 # Specify the input and output folders
 INPUT_FOLDER = 'input_files'
 OUTPUT_FOLDER = 'output_files'
 
 # Paths to the optimized model and scaler files
-MODEL_PATH = os.path.join(OUTPUT_FOLDER, f'optimized_multi_model-{MODEL_ID}.joblib')
-SCALER_PATH = os.path.join(INPUT_FOLDER, 'scaler2_x.joblib')
+SCALER_FILE = 'scaler2_x.joblib'
+MODELS_FILE = 'opt_model-{model_id}-output-{output_number}.joblib'
 
-# Load the optimized model
-try:
-    multi_rf_model = joblib.load(MODEL_PATH)
-    logging.info("Optimized Multi-output Random Forest model loaded successfully.")
-except FileNotFoundError as e:
-    logging.error(f"Model file not found: {MODEL_PATH}")
-    raise e
+# Paths to columns names
+COLUMNS_X_FILE = 'df2_scaled_x_columns.joblib'
+COLUMNS_Y_FILE = 'df2_bin_y_columns.joblib'
 
-# Load the input scaler
-try:
-    input_scaler = joblib.load(SCALER_PATH)
-    logging.info("Input scaler loaded successfully.")
-except FileNotFoundError as e:
-    logging.error(f"Scaler file not found: {SCALER_PATH}")
-    raise e
+# Load the optimized models and input scaler
+models_paths = [
+    os.path.join(OUTPUT_FOLDER, MODELS_FILE.format(model_id=MODEL_ID, output_number=i))
+    for i in range(2)
+]
 
+models = [joblib.load(model) for model in models_paths]
+logging.info("Optimized Models loaded successfully.")
+
+input_scaler = joblib.load(os.path.join(INPUT_FOLDER, SCALER_FILE))
+logging.info("Input scaler loaded successfully.")
+
+
+# Load columns names
+columns_x = joblib.load(os.path.join(INPUT_FOLDER, COLUMNS_X_FILE))
+columns_y = joblib.load(os.path.join(INPUT_FOLDER, COLUMNS_Y_FILE))
+print("Column x: ", columns_x)
+print("Column y: ", columns_y)
+
+# Aspen Plus connection
 aspen_file = r"UTAA_revK.bkp"
 aspen_path = os.path.abspath(aspen_file)
 
@@ -55,6 +64,45 @@ print('Connected!')
 
 Application.InitFromArchive2(aspen_Path)
 Application.visible = 0
+
+
+# Function to preprocess input data and make predictions
+def predict(input_data, input_scaler, models):
+    try:
+        # Scale the input data
+        scaled_input_data = input_scaler.transform(input_data)
+
+        # Initialize lists to store results
+        predicted_probabilities = []
+        predicted_classes = []
+
+        # Loop through each model and make predictions
+        for model in models:
+            # Predict probabilities for the positive class (index 1)
+            probabilities = model.predict_proba(scaled_input_data)[:, 1]
+            predicted_probabilities.append(probabilities)
+
+            # Predict binary classes
+            classes = model.predict(scaled_input_data)
+            predicted_classes.append(classes)
+
+        # Convert results to numpy arrays
+        predicted_probabilities = np.column_stack(predicted_probabilities)
+        predicted_classes = np.column_stack(predicted_classes)
+
+        # Log and return results
+        logging.info(f"Input data shape: {input_data.shape}")
+        logging.info(f"Predicted probabilities shape: {predicted_probabilities.shape}")
+        logging.info(f"Predicted classes shape: {predicted_classes.shape}")
+        logging.info("Prediction completed successfully.")
+
+        return {
+            "predicted_probabilities": predicted_probabilities,
+            "predicted_classes": predicted_classes
+        }
+    except Exception as e:
+        logging.error(f"Error during prediction: {e}")
+        raise e
 
 # Initial guess
 x0 = [0.005, 0.004, 560000, 950000, 0.5] # feedNH3 feedH2S QN1 QN2 SF
@@ -106,17 +154,17 @@ def cost(x_scaled):
     logging.info(f"Total Cost: {total_cost}")
     return total_cost
 
-# Constraint 1 (H2S PPM <= 0.2)
+# Constraint 1 (H2S PPM >= 0.2)
 def constraint1(x_scaled):
     results = predict(x_scaled)
     cH2S_prob = results["predicted_probabilities"][0]
-    return 0.9 - cH2S_prob  # >=0.9
+    return 0.6 - cH2S_prob  # >=0.6
 
-# Constraint 2 (NH3 PPM <= 15)
+# Constraint 2 (NH3 PPM >= 15)
 def constraint2(x_scaled):
     results = predict(x_scaled)
     cNH3_prob = results["predicted_probabilities"][1]
-    return 15 - cNH3_prob  # >=0.9
+    return 0.6 - cNH3_prob  # >=0.6
 
 # Lower bound constraint for QN1
 def bound_QN1_lower(x_scaled):
