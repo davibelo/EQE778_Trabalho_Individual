a5b335b327567456221c180da13fc85338e315f3
Davi Belo <davibelo@gmail.com>
2024-12-20 15:05:51 -0300

    ps1

---
+++ kill_and_simulate.ps1
@@ -0,0 +1,7 @@
+Set-Location -Path "C:\Users\davib\Downloads\EQE778_Trabalho_Individual"
+
+# Activate the .venv environment using Activate.ps1
+.\.venv\Scripts\Activate.ps1
+
+# Run the Python script
+python "5-simulate_loop.py"
